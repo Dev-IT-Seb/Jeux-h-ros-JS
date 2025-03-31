623b2728bf3dd0f633718033e26e1c34c94c6e1b
sebastien c. <schatel.pro@pm.me>
2025-03-31 16:43:25 +0200

    (init) add files HTML CSS and JS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Jeux-heros-JS
-Jeux développé en Javascript
+# Projet-Javascript
+Mon premier projet Javascript  
+04/03/2025
---
+++ script.js
@@ -0,0 +1,93 @@
+// ----------------- GAMES --------------------- //
+//     BIENVENUE SUR LE JEUX D'HARRY POTTER
+// --------------------------------------------- //
+// LANCEMENT DU JEUX AFFICHAGE
+let launchGames = prompt("Veux-tu faire une partie du jeux Harry Potter ? Ecrit 'y' pour OUI ou 'n' pour NON" );
+
+    if (launchGames === "y") {
+    // DEBUT DU JEUX "BIENVENUE - WELCOME"
+    alert(" Bienvenue dans le jeux d'Harry Potter !" );
+    // INFORMATION POUR OUVRIR LA CONSOLE
+    alert("Ouvre ta console de ton navigateur pour voir ton aventure (Raccourci F12)" );
+
+    // ENTRAINEMENT INFORMATIONS
+    console.log(" Debut de l'entrainement de ton héro !" );
+    console.log(" Ton héro doit effectuer une série d'ntrainements avant de partir au combat !" );
+
+    // DEMANDE A L'UTILISATEUR LE NOMBRE DE SEANCE POUR OBTENIR DE L'ENERGIE
+    let nbSeances = prompt("Combien de séances d'entrainement veux-tu réaliser ?")
+
+    // L'UTILISATEUR NE POSSEDE AUCUNE ENERGIE
+    let energie = 0;
+
+    // ENTRAINEMENT -> EN FONCTION DU CHOIX DE L'UTILISATEUR
+    for ( energie; energie <= nbSeances; (energie++) ) {
+        console.log( "Entrainement " + energie + " terminé ! Bravo !" );
+    }
+    // RETIRER 1 POINT POUR COMMENCER A ZERO
+    const heroEnergie = (energie - 1)
+
+    // INFORMATION DU RENDU DE L'ENTRAINEMENT
+    console.log( "Ton entrainement est terminé ! Tu peux désormais combattre contre des ennemis !" );
+    alert(" Tu as obtenu" + " " + heroEnergie + " " + "énergies" );
+    console.log( " Tu as obtenu" + " " + heroEnergie + " " + "énergies" );
+
+    // INFORMATION ENNEMIES EN APPROCHE
+    alert("Harry aperçoit 3 ennemies !")
+
+    // CHOIX DE L'UTILISATEUR
+    let choixUser = prompt("Que veux-tu faire ?  Combattre 'y' ou 'n'" );
+
+    if (choixUser == "y"){
+
+        // COMBAT - CONTRE ENNEMIS
+        console.log( "Lancement du combat !" );
+        console.log( "Il y a 3 ennemies devant toi ! Attention !" );
+
+        // COMBAT - LE JOUEUR PERD TOUTE SON ENERGIE
+        const choixUser = prompt( "Combien d'ennemies veux-tu combattre ?" );
+        // 
+        while (heroEnergie >= choixUser) { 
+            // INFORMATION
+            alert("Le premier ennemie a été abattu ! Bravo !");
+            // REGARDER ENERGIE PDT COMBAT
+            let consultEnergie = prompt( "Veux-tu consulter ton énergie ? Ecrit 'y' pour OUI ou 'n' pour NON" );
+            
+            if (consultEnergie == "y") {
+                const heroEnergiecombat = (heroEnergie - 1)
+                alert("Il te reste " + " "+ heroEnergiecombat + "énergie");
+                console.log("Il te reste " + " " + heroEnergiecombat + " " + "énergie");
+            } else {
+                alert("L'aventure continue !");
+            }
+            // INFORMATION FINAL DU COMBAT
+            alert("Tu as tué les " + choixUser + " ennemis, Bravo !");
+            break;
+        }
+        
+        // RECOMPENSE COFFRES
+        let ObjetCoffre = [ "Epée legendaire", " ", "Potion magique", " ","Bouclier en or" ];
+        // DECLARER VARIABLE AVT DE RENTRER DANS LA BOUCLE -  SINON VARIABLES PAS ACCESSIBLE
+        let recompense = "le héro à récupéré :";
+        // BOUCLE
+        for ( let i = 0; i < ObjetCoffre.length; i++ ){
+        
+            recompense = recompense + ObjetCoffre[i];
+        }
+        // ALERTE
+
+        alert(recompense);
+
+        //---- EN COURS //
+        // INFORMATION ENERGIE APRES COMBAT
+        alert( "Alerte ! Tu n'as plus d'energie !" );
+        alert( "Tu dois refaire un entrainement pour continuer l'aventure !" );
+    } else{
+        alert("Dommage ! Tu aurais pu essayer..." );
+        alert("Relance le jeux si tu veux refaire une partie" );
+    }
+// ARRET DU JEUX
+} else{
+    alert("Reviens quand tu veux, si tu veux faire une partie de ce jeux !")
+}
+// --------------------------------------------- //
